5c37de077f4615153145571f39a21c844642000d
Saran <saran@protecto.ai>
2024-02-14 11:42:59 +0530

    added set-cookies and get-cookies apis

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, HTTPException, Cookie,Request,Response
+from fastapi.middleware.cors import CORSMiddleware
+# from starlette.requests import Request
+from fastapi.responses import JSONResponse
+
+import requests
+app = FastAPI()
+
+# Dictionary to store cookies
+cookies_db = {}
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with your domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/set-cookies")
+async def set_cookie(request: Request,response: Response):
+    params = request.query_params
+    content = {"message": "Cookie set successfully"}
+    response = JSONResponse(content=content)
+    response.set_cookie(key='accesstoken', value=params['accesstoken'])
+    return response
+
+
+@app.get("/get-cookies")
+async def get_cookie(request: Request):
+    """
+    Get the value of the provided cookie key.
+    """
+    if request.cookies.get('accesstoken'):
+        return request.cookies.get('accesstoken')
+    else:
+        raise HTTPException(status_code=404, detail="Cookie not found")
